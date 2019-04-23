c152e9734d424dee04501485afc6ae4154ee5dbe
maxdup <mdupuis@hotmail.ca>
2019-04-23 17:06:44 -0400

    styled map summaries + added icons

--- src/js/directives/map-summary.js
+++ src/js/directives/map-summary.js
@@ -1,10 +1,15 @@
-function map_summary (){
+function map_summary ($sce){
+  "ngInject"
+
   return {
     restrict: 'E',
     replace: true,
     template: require('../../partials/widgets/map-summary.html'),
-    scope: {
-      map: '=',
+    scope: { map: '=', },
+    link: function(scope, element){
+      scope.icon_exclaim = $sce.trustAsHtml(require('octicons')['issue-opened'].toSVG());
+      console.log
+      scope.icon_chevron = $sce.trustAsHtml(require('octicons')['chevron-down'].toSVG());
     }
   }
 }
