8e87e0a5f9c01a2dae34fec64ab55e79d8acae4f
maxdup <mdupuis@hotmail.ca>
2019-04-29 23:49:54 -0400

    made contact page responsive

--- src/js/directives/isotope.js
+++ src/js/directives/isotope.js
@@ -9,17 +9,18 @@ function isotope ($window, $timeout){
     replace: true,
     link: function(scope, element, attrs){
       var rowHeight = 200;
-      var levels = parseInt(element[0].clientHeight / rowHeight);
+      scope.levels = parseInt(element[0].clientHeight / rowHeight);
 
       function getOrder(levelsN){
         if (levelsN <= 2){ return 'lvl2' }
         if (levelsN == 3){ return 'lvl3' }
         if (levelsN >= 4){ return 'lvl4' }
       }
-      var lvln = getOrder(levels);
+      var lvln = getOrder(scope.levels);
       var iso = new Isotope('#'+attrs.id, {
         layoutMode: 'masonryHorizontal',
         itemSelector: '.box',
+        resize: false,
         getSortData: {
           lvl4: '[lvl3] parseInt',
           lvl3: '[lvl3] parseInt',
@@ -32,19 +33,18 @@ function isotope ($window, $timeout){
       });
       scope.reload = function() {
         iso.updateSortData();
-        iso.arrange({'sortBy': getOrder(levels)})
+        iso.arrange({'sortBy': getOrder(scope.levels)})
       };
-      iso.on('layoutComplete', function(filteredItems){
+      angular.element($window).on('resize', function(){
         var newlevels = parseInt(element[0].clientHeight / rowHeight);
-        if (newlevels != levels){
-          levels = newlevels;
+        if (newlevels != scope.levels){
+          scope.levels = newlevels;
           iso.updateSortData();
           iso.arrange({'sortBy': getOrder(newlevels)});
         }
       })
       scope.$on('isotopeReload', scope.reload);
       scope.$on('isotopeLayout', iso.layout);
-
       scope.$on('$destroy', function(){iso.destroy()});
     }
   }
